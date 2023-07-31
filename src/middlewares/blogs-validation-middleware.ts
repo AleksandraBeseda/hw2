8659319ed686671fbd35b83e0391830fb27ddd00
AleksandraBeseda <aleksandra.beseda@clarity.cx>
2023-07-31 18:43:38 +0300

    fixedBlogsValidationMIddleware

--- src/middlewares/blogs-validation-middleware.ts
+++ src/middlewares/blogs-validation-middleware.ts
@@ -4,11 +4,11 @@ import { InputValidationMiddleware } from "./input-validation-middleware";
 const urlPattern ='^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$';
 
 export const BlogsValidationMiddleware = [
-    body("name").trim().isString().isLength({min:1, max: 15}).withMessage("Name should be less then 15 letters"),
+    body("name").isString().trim().isLength({min:1, max: 15}).withMessage("Name should be less then 15 letters"),
 
-    body("description").trim().isString().isLength({min:1, max: 500}).withMessage("Description should be less then 500 letters"),
+    body("description").isString().trim().isLength({min:1, max: 500}).withMessage("Description should be less then 500 letters"),
     
-    body("websiteUrl").isURL().matches(urlPattern).isLength({min:1, max: 100}).withMessage("websiteUrl should be less then 500 letters"),
+    body("websiteUrl").matches(urlPattern).isLength({min:1, max: 100}).withMessage("websiteUrl should be less then 500 letters"),
 
     InputValidationMiddleware
 ]
